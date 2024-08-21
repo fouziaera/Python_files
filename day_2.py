961b7468b9ccc9381198297b45614c6899b40411
majestykoo <fouzia152715@gmail.com>
2024-08-21 21:19:14 +0600

    Code added

---
+++ day_2.py
@@ -0,0 +1,89 @@
+# percentage method
+name= "Era"
+age=19
+"""print("Hello %s My age is %f"% (name,age))
+
+# .format 
+print("Your age is {}. Name is {}".format(age,name))
+
+
+list=[1,2,3,4,5]
+for i in list:
+    print(i,end="\t")"""
+
+
+# first_name = "Mahruf"
+# last_name = "Hossain"
+# name = "Maahi"
+# age = 19.0
+# # f-string
+# print(f"My first name is {first_name}. My last name is {last_name}. My age is {age}.")
+
+# # .format method
+# print("Your age is {}. My name is {}".format(age, name))
+
+# # percent method
+# print("Hello, %s! Your age is %.2f" % (name, age))
+
+
+# a = int(input("Enter the first number:"))
+# b = int(input("Enter the second number :"))
+
+# if a > b:
+#     print(f"{str(a)} is larger than {str(b)}")
+# else:
+#     print(f"{str(b)} is larger than {str(a)}")
+
+
+# name = "Maahi"
+
+# print(name.lower())
+
+# print("maahi".count("a"))
+
+
+# string = "Hello world and world"
+
+# print(string.find("world"))
+
+# print(string.replace("world", "Python"))
+
+
+# while {condition}:
+#     code
+
+# list_2 = [1, 2, 3, 4, 5, 6]
+
+# index = len(list_2) - 1
+# while index >= 0:
+#     print(list_2[index])
+#     index = index - 1
+
+
+# Number of rows in the pattern
+# rows = 5
+
+# # Outer loop for number of rows
+# for i in range(1, rows + 1):
+#     # Inner loop for printing numbers
+#     for j in range(1, i + 1):
+#         print("*", end="")
+#     # Newline after each row
+#     print()
+
+
+# Take input from the user
+# char = input("Enter a character: ")
+
+# # Convert the character to lowercase to handle both upper and lowercase input
+# char = char.capitalize()
+
+# # Check if the input is a single alphabetic character
+# if len(char) == 1 and char.isalpha():
+#     # Check if the character is a vowel
+#     if char in "AEIOU":
+#         print(f"{char} is a vowel.")
+#     else:
+#         print(f"{char} is a consonant.")
+# else:
+#     print("Invalid input. Please enter a single alphabetic character.")
